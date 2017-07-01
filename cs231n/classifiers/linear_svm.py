f714ccc265fef853d21a1970e801327399394eab
foolishguy <asasopo@126.com>
2017-07-01 07:55:29 +0000

    feature

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -20,35 +20,7 @@ def svm_loss_naive(W, X, y, reg):
     - loss as single float
     - gradient with respect to weights W; an array of same shape as W
     """
-    dW = np.zeros(W.shape)
-
-    # compute the loss and the gradient
-    num_classes = W.shape[1]
-    num_train = X.shape[0]
-    loss = 0.0
-    for i in xrange(num_train):
-        scores = X[i].dot(W)
-        correct_class_score = scores[y[i]]
-        for j in xrange(num_classes):
-            if j == y[i]:
-                continue
-                margin = scores[j] - correct_class_score + 1 # note delta = 1
-                if margin > 0:
-                    loss += margin
-                    dW[:,j] += X[i, :]
-                    dW[:,y[i]] -= X[i, :]
-
-                    # Right now the loss is a sum over all training examples, but we want it
-                    # to be an average instead so we divide by num_train.
-                    loss /= num_train
-                    dW /= num_train
-
-    # Add regularization to the loss.
-    loss += reg * np.sum(W * W)
-
-    dW += 2 * reg * W
-
-    return loss, dW
+    return svm_loss_vectorized(W, X, y, reg)
 
 def svm_loss_vectorized(W, X, y, reg):
     """
